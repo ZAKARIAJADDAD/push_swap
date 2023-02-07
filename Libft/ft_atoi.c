ddd4ab7302e5eeef33d61bc395540646e7133037
Zakaria Jaddad <zjaddad@e1r4p10.1337.ma>
2023-02-07 04:47:17 +0100

    push

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zjaddad <zjaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 00:44:42 by zjaddad           #+#    #+#             */
+/*   Updated: 2023/02/06 06:46:49 by zjaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	ft_atoi(const char *s)
+{
+	t_ati	var;
+
+	var.i = 0;
+	var.result = 0;
+	var.sign = 1;
+	while ((s[var.i] == ' ' || (s[var.i] >= 9 && s[var.i] <= 13)))
+		var.i++;
+	if ((s[var.i] == '+' || s[var.i] == '-') && s[var.i + 1] != '\0')
+	{
+		if (s[var.i] == '-')
+			var.sign = -1;
+		var.i++;
+	}
+	while (s[var.i])
+	{
+		if (s[var.i] < '0' || s[var.i] > '9')
+			param_error();
+		var.result = var.result * 10 + (s[var.i] - 48);
+		var.i++;
+	}
+	if (var.result > INT_MAX && var.sign == 1)
+		param_error();
+	if (var.result > 2147483648 && var.sign == -1)
+		param_error();
+	return (var.result * var.sign);
+}
---
+++ Libft/libft_utils.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zjaddad <zjaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/22 02:21:49 by zjaddad           #+#    #+#             */
+/*   Updated: 2023/01/30 17:52:31 by zjaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*plloc;
+	int		i;
+
+	i = 0;
+	plloc = malloc(ft_strlen(s1) + 1);
+	if (!plloc)
+		return (NULL);
+	while (s1[i])
+	{
+		plloc[i] = s1[i];
+		i++;
+	}
+	plloc[i] = '\0';
+	return (plloc);
+}
